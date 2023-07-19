bce32b1b7c931d854ed39f809062525807192b39
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-07-19 20:56:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React,{ Fragment } from 'react';
+import './App.css';
+import Header from './Componant/LAYOUT/Header';
+import Description from './Componant/LAYOUT/Description';
+
+function App() {
+  return (
+    <Fragment className="App">
+     <Header/>
+     <Description/>
+    </Fragment>
+  );
+}
+
+export default App;
